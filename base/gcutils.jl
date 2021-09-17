aa2d1b81d4ffc9ce3fee28a4b2a0d53b09d4b141
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-09-17 10:18:11 -0400

    add doc comment

--- base/gcutils.jl
+++ base/gcutils.jl
@@ -105,6 +105,13 @@ Control whether garbage collection is enabled using a boolean argument (`true` f
 """
 enable(on::Bool) = ccall(:jl_gc_enable, Int32, (Int32,), on) != 0
 
+"""
+    GC.take_heap_snapshot(io::IOStream)
+
+Write a snapshot of the heap, in the JSON format expected by the Chrome
+Devtools Heap Snapshot viewer (.heapsnapshot extension), to the given
+IO stream.
+"""
 function take_heap_snapshot(io)
     ccall(:jl_gc_take_heap_snapshot, Cvoid, (Ptr{Cvoid},), io.handle)
 end
@@ -119,8 +126,7 @@ another Task or thread.
 """
 enable_finalizers(on::Bool) = on ? enable_finalizers() : disable_finalizers()
 
-function enable_finalizers()
-    Base.@inline
+function enable_finalizers() @inline
     ccall(:jl_gc_enable_finalizers_internal, Cvoid, ())
     if Core.Intrinsics.atomic_pointerref(cglobal(:jl_gc_have_pending_finalizers, Cint), :monotonic) != 0
         ccall(:jl_gc_run_pending_finalizers, Cvoid, (Ptr{Cvoid},), C_NULL)

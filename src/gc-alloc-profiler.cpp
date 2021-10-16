3c7b9ae282780944e3e02cb4412638b5db3366fb
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-10-16 00:55:34 -0500

    finally, setup for inserting into trie

--- src/gc-alloc-profiler.cpp
+++ src/gc-alloc-profiler.cpp
@@ -34,8 +34,11 @@ struct StackTrieNode {
 struct AllocProfile {
     StackTrieNode root;
     unordered_map<size_t, string> type_name_by_address;
+};
 
-    vector<Alloc> allocs;
+struct RawBacktrace {
+    jl_bt_element_t *data;
+    size_t size;
 };
 
 // Insert a record into the trie indicating that we allocated the
@@ -43,26 +46,18 @@ struct AllocProfile {
 //
 // TODO: move to method on StackTrieNode
 // I don't know how to C++
-void record_alloc(AllocProfile *profile, vector<StackFrame> stack, size_t type_address) {
-    profile->allocs.push_back(Alloc{
-        type_address,
-        stack
-    });
+void record_alloc(AllocProfile *profile, RawBacktrace stack, size_t type_address) {
+    vector<jl_bt_element_t*> stack_vec;
+
+    for (int i = 0; i < stack.size; i += jl_bt_entry_size(stack.data + i)) {
+        jl_bt_element_t *bt_entry = stack.data + i;
+
+        stack_vec.push_back(bt_entry);
+    }
 }
 
 void alloc_profile_serialize(ios_t *out, AllocProfile *profile) {
-    for (auto alloc : profile->allocs) {
-        ios_printf(
-            out, "type: %s\n",
-            profile->type_name_by_address[alloc.type_address].c_str()
-        );
-        for (auto frame : alloc.stack) {
-            ios_printf(
-                out, "  %s at %s:%d\n",
-                frame.func_name.c_str(), frame.file_name.c_str(), frame.line_no
-            );
-        }
-    }
+    ios_printf(out, "TODO: serialize trie\n");
 }
 
 // == global variables manipulated by callbacks ==
@@ -158,69 +153,17 @@ StackFrame get_native_frame(uintptr_t ip) JL_NOTSAFEPOINT {
     return out_frame;
 }
 
-vector<StackFrame> get_stack() {
+RawBacktrace get_stack() {
     // TODO: don't allocate this every time
     jl_bt_element_t *bt_data = (jl_bt_element_t*) malloc(JL_MAX_BT_SIZE);
 
     // TODO: tune the number of frames that are skipped
     size_t bt_size = rec_backtrace(bt_data, JL_MAX_BT_SIZE, 1);
 
-    vector<StackFrame> stack;
-
-    for (int i = 0; i < bt_size; i += jl_bt_entry_size(bt_data + i)) {
-        jl_bt_element_t *bt_entry = bt_data + i;
-
-        // jl_print_bt_entry_codeloc(bt_entry);
-
-        if (jl_bt_is_native(bt_entry)) {
-            stack.push_back(get_native_frame(bt_entry[0].uintptr));
-            continue;
-        }
-
-        // adapted from jl_print_bt_entry_codeloc
-        size_t ip = jl_bt_entry_header(bt_entry);
-        jl_value_t *code = jl_bt_entry_jlvalue(bt_entry, 0);
-        if (jl_is_method_instance(code)) {
-            // When interpreting a method instance, need to unwrap to find the code info
-            code = ((jl_method_instance_t*)code)->uninferred;
-        }
-        if (jl_is_code_info(code)) {
-            jl_code_info_t *src = (jl_code_info_t*)code;
-            // See also the debug info handling in codegen.cpp.
-            // NB: debuginfoloc is 1-based!
-            intptr_t debuginfoloc = ((int32_t*)jl_array_data(src->codelocs))[ip];
-            while (debuginfoloc != 0) {
-                jl_line_info_node_t *locinfo = (jl_line_info_node_t*)
-                    jl_array_ptr_ref(src->linetable, debuginfoloc - 1);
-                assert(jl_typeis(locinfo, jl_lineinfonode_type));
-                const char *func_name = "Unknown";
-                jl_value_t *method = locinfo->method;
-                if (jl_is_method_instance(method))
-                    method = ((jl_method_instance_t*)method)->def.value;
-                if (jl_is_method(method))
-                    method = (jl_value_t*)((jl_method_t*)method)->name;
-                if (jl_is_symbol(method))
-                    func_name = jl_symbol_name((jl_sym_t*)method);
-
-                stack.push_back(StackFrame{
-                    func_name,
-                    jl_symbol_name(locinfo->file),
-                    locinfo->line,
-                });
-                
-                debuginfoloc = locinfo->inlined_at;
-            }
-        }
-        else {
-            // If we're using this function something bad has already happened;
-            // be a bit defensive to avoid crashing while reporting the crash.
-            jl_safe_printf("No code info - unknown interpreter state!\n");
-        }
-    }
-
-    free(bt_data);
-
-    return stack;
+    return RawBacktrace{
+        bt_data,
+        bt_size
+    };
 }
 
 void _record_allocated_value(jl_value_t *val) {
@@ -231,6 +174,9 @@ void _record_allocated_value(jl_value_t *val) {
     auto stack = get_stack();
 
     record_alloc(g_alloc_profile, stack, (size_t)type);
+
+    // TODO: more idiomatic way to destruct this
+    free(stack.data);
 }
 
 void _report_gc_started() {
@@ -249,6 +195,5 @@ void _report_gc_finished(uint64_t pause, uint64_t freed, uint64_t allocd) {
     if (g_alloc_profile_out != nullptr) {
         alloc_profile_serialize(g_alloc_profile_out, g_alloc_profile);
         ios_flush(g_alloc_profile_out);
-        g_alloc_profile->allocs.clear();
     }
 }

d4079dde4684fcff079698960f8ce4618be019cb
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-10-22 06:49:56 -0400

    only capture actual julia frames

--- src/gc-alloc-profiler.cpp
+++ src/gc-alloc-profiler.cpp
@@ -17,6 +17,8 @@ struct StackFrame {
     string func_name;
     string file_name;
     intptr_t line_no;
+
+    string total;
 };
 
 struct Alloc {
@@ -34,10 +36,53 @@ struct AllocProfile {
     unordered_map<size_t, string> type_name_by_address;
 };
 
-struct RawBacktrace {
-    jl_bt_element_t *data;
-    size_t size;
-};
+// == utility functions ==
+
+// https://stackoverflow.com/questions/874134/find-out-if-string-ends-with-another-string-in-c
+bool ends_with(string const &full_string, string const &ending) {
+    if (full_string.length() >= ending.length()) {
+        return (0 == full_string.compare(full_string.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+bool starts_with(string const &full_string, string const &beginning) {
+    if (full_string.length() >= beginning.length()) {
+        return (0 == full_string.compare(0, beginning.length(), beginning));
+    } else {
+        return false;
+    }
+}
+
+string _type_as_string(jl_datatype_t *type) {
+    if ((uintptr_t)type < 4096U) {
+        return "<corrupt>";
+    } else if (type == (jl_datatype_t*)jl_buff_tag) {
+        return "<buffer>";
+    } else if (type == (jl_datatype_t*)jl_malloc_tag) {
+        return "<malloc>";
+    } else if (type == jl_string_type) {
+        return "<string>";
+    } else if (type == jl_symbol_type) {
+        return "<symbol>";
+    } else if (jl_is_datatype(type)) {
+        ios_t str_;
+        ios_mem(&str_, 10024);
+        JL_STREAM* str = (JL_STREAM*)&str_;
+
+        jl_static_show(str, (jl_value_t*)type);
+
+        string type_str = string((const char*)str_.buf, str_.size);
+        ios_close(&str_);
+
+        return type_str;
+    } else {
+        return "<missing>";
+    }
+}
+
+// === stack stuff ===
 
 vector<StackFrame> get_julia_frames(jl_bt_element_t *bt_entry) {
     vector<StackFrame> ret;
@@ -83,8 +128,8 @@ vector<StackFrame> get_julia_frames(jl_bt_element_t *bt_entry) {
     return ret;
 }
 
-StackFrame get_native_frame(uintptr_t ip) JL_NOTSAFEPOINT {
-    StackFrame out_frame;
+vector<StackFrame> get_native_frames(uintptr_t ip) JL_NOTSAFEPOINT {
+    vector<StackFrame> out_frames;
 
     // This function is not allowed to reference any TLS variables since
     // it can be called from an unmanaged thread on OSX.
@@ -96,12 +141,15 @@ StackFrame get_native_frame(uintptr_t ip) JL_NOTSAFEPOINT {
     for (i = 0; i < n; i++) {
         jl_frame_t frame = frames[i];
         if (!frame.func_name) {
-            jl_safe_printf("unknown function (ip: %p)\n", (void*)ip);
+            // TODO: record these somewhere
+            // jl_safe_printf("unknown function (ip: %p)\n", (void*)ip);
         }
         else {
-            out_frame.func_name = frame.func_name;
-            out_frame.file_name = frame.file_name;
-            out_frame.line_no = frame.line;
+            out_frames.push_back(StackFrame{
+                frame.func_name,
+                frame.file_name,
+                frame.line,
+            });
 
             free(frame.func_name);
             free(frame.file_name);
@@ -109,37 +157,70 @@ StackFrame get_native_frame(uintptr_t ip) JL_NOTSAFEPOINT {
     }
     free(frames);
 
-    return out_frame;
+    return out_frames;
 }
 
-string entry_to_string(jl_bt_element_t *entry) {
-    ios_t str;
-    ios_mem(&str, 10024);
+vector<StackFrame> get_stack() {
+    // TODO: don't allocate this every time
+    jl_bt_element_t *bt_data = (jl_bt_element_t*) malloc(JL_MAX_BT_SIZE);
+
+    // TODO: tune the number of frames that are skipped
+    size_t bt_size = rec_backtrace(bt_data, JL_MAX_BT_SIZE, 1);
+
+    vector<StackFrame> out;
+
+    for (int i = 0; i < bt_size; i += jl_bt_entry_size(bt_data + i)) {
+        jl_bt_element_t *entry = bt_data + i;
+        auto is_native = jl_bt_is_native(entry);;
+
+        // TODO: cache frames by bt_element as string?
+        auto frames = is_native
+            ? get_native_frames(entry[0].uintptr)
+            : get_julia_frames(entry);
         
-    if (jl_bt_is_native(entry)) {
-        auto frame = get_native_frame(entry[0].uintptr);
-        ios_printf(
-            &str, "%s at %s:%d",
-            frame.func_name.c_str(), frame.file_name.c_str(), frame.line_no
-        );
-    } else {
-        auto frames = get_julia_frames(entry);
         for (auto frame : frames) {
-            ios_printf(
-                &str, "%s at %s:%d",
-                frame.func_name.c_str(), frame.file_name.c_str(), frame.line_no
-            );
-        }    
+            auto frame_label = frame.func_name;
+            auto is_julia = ends_with(frame.file_name, ".jl") || frame.file_name == "top-level scope";
+            auto actual_is_native = !is_julia;
+            auto is_stdlib = is_julia && starts_with(frame.file_name, "./");
+
+            if (actual_is_native || is_stdlib) {
+                continue;
+            }
+
+            out.push_back(frame);
+        }
     }
 
-    string ret = string((const char*)str.buf, str.size);
-    ios_close(&str);
+    free(bt_data);
 
-    return ret;
+    return out;
 }
 
+string stack_frame_to_string(StackFrame frame) {
+    if (frame.total != "") {
+        return frame.total;
+    }
+
+    ios_t str;
+    ios_mem(&str, 1024);
+
+    ios_printf(
+        &str, "%s at %s:%d",
+        frame.func_name.c_str(), frame.file_name.c_str(), frame.line_no
+    );
+
+    string type_str = string((const char*)str.buf, str.size);
+    frame.total = type_str;
+    ios_close(&str);
+
+    return frame.total;
+}
+
+// === trie stuff ===
+
 // TODO: pass size as well
-void trie_insert(StackTrieNode *node, vector<jl_bt_element_t*> path, size_t idx, size_t type_address) {
+void trie_insert(StackTrieNode *node, vector<StackFrame> path, size_t idx, size_t type_address) {
     if (idx == path.size()) {
         auto allocs = node->allocs_by_type_address.find(type_address);
         if (allocs == node->allocs_by_type_address.end()) {
@@ -150,8 +231,8 @@ void trie_insert(StackTrieNode *node, vector<jl_bt_element_t*> path, size_t idx,
         return;
     }
     
-    auto entry = path[idx];
-    string child_str = entry_to_string(entry);
+    auto frame = path[idx];
+    string child_str = stack_frame_to_string(frame);
     auto child = node->children.find(child_str);
     StackTrieNode *child_node;
     if (child == node->children.end()) {
@@ -161,23 +242,6 @@ void trie_insert(StackTrieNode *node, vector<jl_bt_element_t*> path, size_t idx,
         child_node = child->second;
     }
     trie_insert(child_node, path, idx+1, type_address);
-}
-
-// Insert a record into the trie indicating that we allocated the
-// given type at the given stack.
-//
-// TODO: move to method on StackTrieNode
-// I don't know how to C++
-void record_alloc(AllocProfile *profile, RawBacktrace stack, size_t type_address) {
-    vector<jl_bt_element_t*> stack_vec;
-
-    for (int i = 0; i < stack.size; i += jl_bt_entry_size(stack.data + i)) {
-        jl_bt_element_t *bt_entry = stack.data + i;
-
-        stack_vec.push_back(bt_entry);
-    }
-
-    trie_insert(&profile->root, stack_vec, 0, type_address);
 }
 
 void print_indent(ios_t *out, int level) {
@@ -209,35 +273,6 @@ void alloc_profile_serialize(ios_t *out, AllocProfile *profile) {
 AllocProfile *g_alloc_profile;
 ios_t *g_alloc_profile_out;
 
-// == utility functions ==
-
-string _type_as_string(jl_datatype_t *type) {
-    if ((uintptr_t)type < 4096U) {
-        return "<corrupt>";
-    } else if (type == (jl_datatype_t*)jl_buff_tag) {
-        return "<buffer>";
-    } else if (type == (jl_datatype_t*)jl_malloc_tag) {
-        return "<malloc>";
-    } else if (type == jl_string_type) {
-        return "<string>";
-    } else if (type == jl_symbol_type) {
-        return "<symbol>";
-    } else if (jl_is_datatype(type)) {
-        ios_t str_;
-        ios_mem(&str_, 10024);
-        JL_STREAM* str = (JL_STREAM*)&str_;
-
-        jl_static_show(str, (jl_value_t*)type);
-
-        string type_str = string((const char*)str_.buf, str_.size);
-        ios_close(&str_);
-
-        return type_str;
-    } else {
-        return "<missing>";
-    }
-}
-
 // == exported interface ==
 
 JL_DLLEXPORT void jl_start_alloc_profile(ios_t *stream) {
@@ -265,19 +300,6 @@ void register_type_string(jl_datatype_t *type) {
     g_alloc_profile->type_name_by_address[(size_t)type] = type_str;
 }
 
-RawBacktrace get_stack() {
-    // TODO: don't allocate this every time
-    jl_bt_element_t *bt_data = (jl_bt_element_t*) malloc(JL_MAX_BT_SIZE);
-
-    // TODO: tune the number of frames that are skipped
-    size_t bt_size = rec_backtrace(bt_data, JL_MAX_BT_SIZE, 1);
-
-    return RawBacktrace{
-        bt_data,
-        bt_size
-    };
-}
-
 void _record_allocated_value(jl_value_t *val) {
     auto type = (jl_datatype_t*)jl_typeof(val);
     register_type_string(type);
@@ -285,10 +307,7 @@ void _record_allocated_value(jl_value_t *val) {
     // TODO: get stack, push into vector
     auto stack = get_stack();
 
-    record_alloc(g_alloc_profile, stack, (size_t)type);
-
-    // TODO: more idiomatic way to destruct this
-    free(stack.data);
+    trie_insert(&g_alloc_profile->root, stack, 0, (size_t) type);
 }
 
 void _report_gc_started() {

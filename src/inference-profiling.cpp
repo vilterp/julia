2105b18f2bf3a16bf6b2c9b32916697710e7032f
Nathan Daly <nhdaly@gmail.com>
2022-11-18 12:20:36 -0700

    Fix bug: Use julia Array, not C++ vec to keep elements rooted

--- src/inference-profiling.cpp
+++ src/inference-profiling.cpp
@@ -10,7 +10,8 @@ using std::vector;
 jl_mutex_t typeinf_profiling_lock;
 
 // Guarded by jl_typeinf_profiling_lock.
-vector<jl_value_t*> inference_profiling_results;
+// A julia Vector{jl_value_t}
+jl_array_t* inference_profiling_results_array;
 
 // == exported interface ==
 
@@ -20,18 +21,23 @@ JL_DLLEXPORT jl_array_t* jl_typeinf_profiling_clear_and_fetch(jl_value_t *array_
 {
     JL_LOCK(&typeinf_profiling_lock);
 
-    size_t len = inference_profiling_results.size();
+    if (inference_profiling_results_array == nullptr) {
+        // Return an empty array
+        return jl_alloc_array_1d(array_timing_type, 0);
+    }
+
+    size_t len = jl_array_len(inference_profiling_results_array);
 
     jl_array_t *out = jl_alloc_array_1d(array_timing_type, len);
     JL_GC_PUSH1(&out);
 
-    memcpy(out->data, inference_profiling_results.data(), len * sizeof(void*));
+    memcpy(out->data, inference_profiling_results_array->data, len * sizeof(void*));
 
-    inference_profiling_results.clear();
+    jl_array_del_end(inference_profiling_results_array, len);
 
     JL_UNLOCK(&typeinf_profiling_lock);
-    JL_GC_POP();
 
+    JL_GC_POP();
     return out;
 }
 
@@ -39,7 +45,11 @@ JL_DLLEXPORT void jl_typeinf_profiling_push_timing(jl_value_t *timing)
 {
     JL_LOCK(&typeinf_profiling_lock);
 
-    inference_profiling_results.push_back(timing);
+    if (inference_profiling_results_array == nullptr) {
+        inference_profiling_results_array = jl_alloc_array_1d(jl_array_any_type, 0);
+    }
+
+    jl_array_ptr_1d_push(inference_profiling_results_array, timing);
 
     JL_UNLOCK(&typeinf_profiling_lock);
 }
